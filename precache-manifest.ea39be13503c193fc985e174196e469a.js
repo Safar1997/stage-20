490325c36bf58d97907eab6d8f6fb37c50212462
Safar1997 <kurbonov_safar@bk.ru>
2020-04-13 12:30:42 +0300

    Updates

---
+++ precache-manifest.ea39be13503c193fc985e174196e469a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb3e1c87b53f48713e32fc7a54968df",
+    "url": "/stage-20/index.html"
+  },
+  {
+    "revision": "af6700fdc223b9a10145",
+    "url": "/stage-20/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "af6700fdc223b9a10145",
+    "url": "/stage-20/static/js/2.4ba9b5e0.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/stage-20/static/js/2.4ba9b5e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe4a0a3fab8d09ffe5d",
+    "url": "/stage-20/static/js/main.28033e9f.chunk.js"
+  },
+  {
+    "revision": "5c59e5592b35baaf3228",
+    "url": "/stage-20/static/js/runtime-main.ea156473.js"
+  }
+]);
